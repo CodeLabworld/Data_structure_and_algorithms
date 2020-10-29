b423a9ef35577d2a63e6019d10dfbf2d1837bfc6
CodeLabworld <1103637169@qq.com>
2020-10-29 09:33:30 +0800

    更新

--- 栈/main.c
+++ 栈/main.c
@@ -4,7 +4,76 @@
 
 int main()
 {
-    Linkstack S; //定义栈
-    //初始化栈
-    Initialization_linkstack(S); //采用引用传参，若用指针需要定义头结点，因为c语言中不能用空指针，会报错
+    int command_one; //一级命令
+    int command_two; //二级命令
+    while (1)
+    {
+        printf("___________________________________________\n");
+        printf("1.链式存储结构栈\n2.顺序存储结构栈\n");
+        printf("___________________________________________\n");
+        printf("输入执行命令：");
+        scanf("%d", &command_one);
+        if (command_one == 1)
+        {
+            //定义栈
+            Linkstack S;
+            //初始化栈
+            Initialization_linkstack(S); //采用引用传参，若用指针需要定义头结点，因为c语言中不能用空指针，会报错
+            while (1)
+            {
+                printf("___________________________________________\n");
+                printf("1.入栈\n2.出栈\n");
+                printf("___________________________________________\n");
+                printf("输入操作命令：");
+                scanf("%d", &command_two);
+                if (command_two == 1)
+                {
+                    //链式存储结构入栈
+                    LinkPush(S);
+                }
+                else if (command_two == 2)
+                {
+                    //链式存储结构出栈
+                    Linkpop(S);
+                }
+                else
+                {
+                    printf("输入命令有误，请重新输入！\n");
+                }
+            }
+        }
+        else if (command_one == 2)
+        {
+            //定义栈
+            SqStack Q;
+            //初始化栈
+            Initialization_Sqstack(Q); //采用引用
+            while (1)
+            {
+                printf("___________________________________________\n");
+                printf("1.入栈\n2.出栈\n");
+                printf("___________________________________________\n");
+                printf("输入操作命令：");
+                scanf("%d", &command_two);
+                if (command_two == 1)
+                {
+                    //顺序存储结构入栈
+                    SqStack_push(Q);
+                }
+                else if (command_two == 2)
+                {
+                    //顺序存储结构出栈
+                    SqStack_pop(Q);
+                }
+                else
+                {
+                    printf("输入命令有误，请重新输入！\n");
+                }
+            }
+        }
+        else
+        {
+            printf("输入命令错误，请重新输入\n");
+        }
+    }
 }
--- 栈/stack.h
+++ 栈/stack.h
@@ -2,6 +2,7 @@
 #include <malloc.h>
 #include <stdlib.h>
 
+//链式存储结构
 typedef struct StackNode //定义栈中结点
 {
     int data;               //数据
@@ -14,13 +15,47 @@ typedef struct Linkstack //定义整个链式栈
     int count;        //栈内数据个数
 } Linkstack;
 
+//顺序存储结构
+#define MAXSIZE 20
+typedef struct SqStack
+{
+    int data[MAXSIZE]; //数据数组
+    int top;           //栈顶标识
+} SqStack;
+
+//链式存储结构显示函数
+//----------------------------------
+void LinkShow(Linkstack &S)
+{
+    int i;
+    Linkstackptr p;
+    p = S.top;
+    printf("栈内数据如下:\n");
+    for (i = 0; i < S.count; i++)
+    {
+        printf("|%d|\n", p->data);
+        p = p->next;
+    }
+}
+//顺序存储结构显示函数
+//------------------------------
+void SqStackShow(SqStack &Q)
+{
+    printf("栈内数据显示如下：\n");
+    int i;
+    for (i = Q.top; i >= 0; i--)
+    {
+        printf("|%d|\n", Q.data[i]);
+    }
+}
+
 //链式存储结构栈的初始化
+//-----------------------------------------------------
 void Initialization_linkstack(Linkstack &S)
 {
     int num, i;
     printf("输入要输入数据的个数：");
     scanf("%d", &num);
-    printf("\n");
     printf("输入数据：");
     for (i = 0; i < num; i++)
     {
@@ -37,29 +72,76 @@ void Initialization_linkstack(Linkstack &S)
         S.top = s;
     }
     S.count = num;
-    printf("初始化完成\n");
-    LinkShow(S)
+    printf("----初始化完成----\n");
+    LinkShow(S);
 }
+
 // //链式存储结构入栈函数
-// void linklist_push()
-// //链式存储结构出栈函数
-// void linklist_pop()
-// //链式存储结构显示函数
-void LinkShow(Linkstack &S)
+//--------------------------------------------------
+void LinkPush(Linkstack &S)
 {
-    int i;
-    Linkstackptr p;
-    p = S.top;
-    printf("栈内数据如下:\n");
-    for (i = 0; i < S.count; i++)
+    Linkstackptr s = (Linkstackptr)malloc(sizeof(StackNode));
+    printf("输入入栈的数据：");
+    scanf("%d", &s->data);
+    s->next = S.top;
+    S.top = s;
+    S.count++;
+    printf("----入栈成功----\n");
+    LinkShow(S);
+}
+
+//链式存储结构出栈函数
+//------------------------------------------------
+void Linkpop(Linkstack &S)
+{
+    S.top = S.top->next;
+    S.count--;
+    printf("----出栈成功----\n");
+    LinkShow(S);
+}
+//顺序存储结构栈的初始化
+void Initialization_Sqstack(SqStack &Q)
+{
+    int i, num;
+    printf("输入栈内数据个数：");
+    scanf("%d", &num);
+    printf("输入栈内数据：");
+
+    for (i = 0; i < num; i++)
     {
-        printf("|%d|\n", p->data);
-        p = p->next;
+        scanf("%d", &Q.data[i]);
+    }
+    Q.top = i - 1;
+    printf("初始化完成\n");
+    SqStackShow(Q);
+}
+//顺序存储结构入栈函数
+void SqStack_push(SqStack &Q)
+{
+    if (Q.top == MAXSIZE - 1)
+    {
+        printf("栈满\n");
+    }
+    else
+    {
+        printf("输入入栈数据：");
+        scanf("%d", &Q.data[Q.top + 1]);
+        Q.top++;
+        printf("入栈成功\n");
+        SqStackShow(Q);
     }
 }
-// //顺序存储结构栈的初始化
-// void Initialization_sqstack()
-// //顺序存储结构入栈函数
-// void sqstack_push()
-// //顺序存储结构出栈函数
-// void sqstack_pop()
+//顺序存储结构出栈函数
+void SqStack_pop(SqStack &Q)
+{
+    if (Q.top == -1)
+    {
+        printf("栈为空\n");
+    }
+    else
+    {
+        Q.top--;
+        printf("出栈成功\n");
+        SqStackShow(Q);
+    }
+}
